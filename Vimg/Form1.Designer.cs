575a10a8d2a676a748321bcce5a13137bf01f0e1
aimenlabed <114228669+aimenlabed@users.noreply.github.com>
2023-02-07 14:39:45 +0100

    Add files via upload
     source code

---
+++ Vimg/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace Vimg
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(599, 447);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(330, 53);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(219, 75);
+            this.flowLayoutPanel1.TabIndex = 2;
+            this.flowLayoutPanel1.Visible = false;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = " Image info";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Vimg";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Vimg/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Vimg
+{
+   
+    public partial class Form1 : Form
+    { 
+        Bitmap b;
+        Bitmap bo;
+        string su;
+        bool w;
+        float tw;
+        float th;
+        bool N;
+        
+        public Form1()
+        {
+            
+            this.SizeChanged += Form1_SizeChanged;
+            
+            InitializeComponent();
+            
+        }
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button==MouseButtons.Left) {
+                if (flowLayoutPanel1.Visible == false) pictur();
+                 else flowLayoutPanel1.Visible = false;
+            }
+            else if (e.Button == MouseButtons.Right)m(); 
+        }
+
+        private void m()
+        {
+            Point p = Cursor.Position;
+            
+            p.X -= Location.X+10;
+            p.Y -= Location.Y + 35;
+            if (p.X > Width - 230) p.X -= 219;
+            if (p.Y > Height - 100) p.Y -= 75;
+            flowLayoutPanel1.Location = p;
+            flowLayoutPanel1.Visible = true;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Width = this.Width;
+            pictureBox1.Height = this.Height;
+            
+            if (bo != null) {
+            float cc = (float)bo.Width / (float)bo.Height;
+            float c2 = (float)pictureBox1.Width / (float)pictureBox1.Height;
+            if (pictureBox1.Width >= pictureBox1.Height)
+            {
+                if (bo.Width >= bo.Height)
+                {
+                    if (cc < c2)
+                    {
+                        b = new Bitmap(bo, new Size((pictureBox1.Height * bo.Width / bo.Height), pictureBox1.Height));
+                    }
+                    else
+                    {
+                        b = new Bitmap(bo, new Size(pictureBox1.Width, (pictureBox1.Width * bo.Height / bo.Width)));
+                    }
+                }
+                else
+                    b = new Bitmap(bo, new Size((pictureBox1.Height * bo.Width / bo.Height), pictureBox1.Height));
+            }
+            else
+            {
+
+                if (bo.Width < bo.Height)
+                {
+                    if (cc > c2)
+                        b = new Bitmap(bo, new Size(pictureBox1.Width, (pictureBox1.Width * bo.Height / bo.Width)));
+                    else
+                        b = new Bitmap(bo, new Size((pictureBox1.Height * bo.Width / bo.Height), pictureBox1.Height));
+                }
+                else
+                    b = new Bitmap(bo, new Size(pictureBox1.Width, (pictureBox1.Width * bo.Height / bo.Width)));
+            }
+                pictureBox1.Image = b;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        { }
+        private void pictur()
+        {
+
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.png;*.jpeg;*.gif;*.bmp;*.jpg)|*.png;*.jpeg;*.gif;*.bmp;*.jpg";
+            if (open.ShowDialog() == DialogResult.OK) {
+                su = open.FileName;
+             bo =  new Bitmap(open.FileName);
+                float cc = (float)bo.Width / (float)bo.Height;
+                float c2 = (float)pictureBox1.Width / (float)pictureBox1.Height;
+                if (pictureBox1.Width >= pictureBox1.Height)
+                {
+                    if (bo.Width >= bo.Height)
+                    {
+                        if (cc < c2)
+                        {
+                            b = new Bitmap(bo, new Size((pictureBox1.Height * bo.Width / bo.Height), pictureBox1.Height));
+                        }
+                        else
+                        {
+                            b = new Bitmap(bo, new Size(pictureBox1.Width, (pictureBox1.Width * bo.Height / bo.Width)));
+                        }
+                    }
+                    else
+                        b = new Bitmap(bo, new Size((pictureBox1.Height * bo.Width / bo.Height), pictureBox1.Height));
+                }
+                else
+                {
+
+                    if (b.Width < b.Height)
+                    {
+                        if (cc > c2)
+                            b = new Bitmap(bo, new Size(pictureBox1.Width, (pictureBox1.Width * bo.Height / bo.Width)));
+                        else
+                            b = new Bitmap(bo, new Size((pictureBox1.Height * bo.Width / bo.Height), pictureBox1.Height));
+                    }
+                    else
+                        b = new Bitmap(bo, new Size(pictureBox1.Width, (pictureBox1.Width * bo.Height / bo.Width)));
+                }
+
+
+                pictureBox1.Image = b;
+            }
+            pictureBox1.BackColor = Color.Black;
+                //this.Width = this.Width / 2;
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   if(N==false)
+            if(Program.U.Length>0)
+            {
+                    su = Program.U[0];
+                b = new Bitmap(Program.U[0]);
+                    b = new Bitmap(b, new Size(pictureBox1.Width, pictureBox1.Height));
+                    pictureBox1.Image = b;
+
+            }
+            N = true;
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Visible = false;
+            pictur();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Visible = false;
+            if (bo == null) MessageBox.Show("no Image");
+            else MessageBox.Show("Image Url : "+su+ "\n\nHeight    : " + bo.Height.ToString()+ "\nWidth     : "+bo.Width.ToString());
+        }
+    }
+}
